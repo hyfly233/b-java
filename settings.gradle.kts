9cfca7072e1cc6a2fe57d3571efb212a4372aca4
hyfly233 <hyfly233@outlook.com>
2023-11-13 12:00:00 +0800

    commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,5 @@ rootProject.name = "b-java"
 include("thread")
 include("jvm", "jvm:asm", "jvm:classloader", "jvm:memory")
 include("spring-demo", "spring-demo:event-demo", "spring-demo:kafka-demo")
+
+include("spring-demo:poi-tl-demo")
--- spring-demo/build.gradle.kts
+++ spring-demo/build.gradle.kts
@@ -1,15 +1,3 @@
 plugins {
     id("java")
 }
-
-group = "com.hyfly"
-version = "1.0-SNAPSHOT"
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
