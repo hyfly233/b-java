fa6998d3f4ae08c3539e5cf5c9d1cddca46676e8
更科瑠夏 <33020548+hyfly233@users.noreply.github.com>
2024-07-04 16:54:38 +0800

    Delete spring/actuator directory

--- spring/actuator/src/main/java/com/hyfly/actuator/entity/message/constants/MessageLevel.java
+++
@@ -1,8 +0,0 @@
-package com.hyfly.actuator.entity.message.constants;
-
-public class MessageLevel {
-
-    public static final String INFO = "info";
-
-    public static final String ERROR = "error";
-}
