09ab21970fbec3bfb42b4257de8e69b99f081c68
hyfly233 <hyfly233@outlook.com>
2025-01-13 08:56:24 +0800

    commit

--- dolphinscheduler/build.gradle.kts
+++ dolphinscheduler/build.gradle.kts
@@ -7,10 +7,13 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     // dolphinscheduler
-//    implementation("com.github.weaksloth:dolphinscheduler-sdk-java:3.2.0-RELEASE")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation("org.apache.httpcomponents:httpmime:4.5.13")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.10")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
+    implementation("org.apache.httpcomponents:httpmime:4.5.13") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
     implementation("com.google.guava:guava:31.1-jre")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
