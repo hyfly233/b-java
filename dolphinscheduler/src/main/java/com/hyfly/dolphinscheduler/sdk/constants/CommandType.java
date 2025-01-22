ef7f8c9696f907899345cc639d5e3eed841b1695
hyfly233 <hyfly233@outlook.com>
2025-01-22 10:40:00 +0800

    commit

---
+++ dolphinscheduler/src/main/java/com/hyfly/dolphinscheduler/sdk/constants/CommandType.java
@@ -0,0 +1,35 @@
+package com.hyfly.dolphinscheduler.sdk.constants;
+
+/**
+ * 命令类型
+ */
+public class CommandType {
+    public static final String START_PROCESS = "START_PROCESS";
+
+    public static final String START_CURRENT_TASK_PROCESS = "START_CURRENT_TASK_PROCESS";
+
+    public static final String RECOVER_TOLERANCE_FAULT_PROCESS = "RECOVER_TOLERANCE_FAULT_PROCESS";
+
+    public static final String RECOVER_SUSPENDED_PROCESS = "RECOVER_SUSPENDED_PROCESS";
+
+    public static final String START_FAILURE_TASK_PROCESS = "START_FAILURE_TASK_PROCESS";
+
+    public static final String COMPLEMENT_DATA = "COMPLEMENT_DATA";
+
+    public static final String SCHEDULER = "SCHEDULER";
+
+    public static final String REPEAT_RUNNING = "REPEAT_RUNNING";
+
+    public static final String PAUSE = "PAUSE";
+
+    public static final String STOP = "STOP";
+
+    public static final String RECOVER_WAITING_THREAD = "RECOVER_WAITING_THREAD";
+
+    public static final String RECOVER_SERIAL_WAIT = "RECOVER_SERIAL_WAIT";
+
+    public static final String EXECUTE_TASK = "EXECUTE_TASK";
+
+    public static final String DYNAMIC_GENERATION = "DYNAMIC_GENERATION";
+    
+}
