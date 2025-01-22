4cf475d215c3cf29aca922e8671a8cef05f285f9
hyfly233 <hyfly233@outlook.com>
2025-01-22 10:40:03 +0800

    commit

---
+++ dolphinscheduler/src/main/java/com/hyfly/dolphinscheduler/sdk/constants/FailureStrategy.java
@@ -0,0 +1,12 @@
+package com.hyfly.dolphinscheduler.sdk.constants;
+
+/**
+ * 任务失败策略
+ */
+public final class FailureStrategy {
+
+    public static final String END = "END";
+
+    public static final String CONTINUE = "CONTINUE";
+
+}
